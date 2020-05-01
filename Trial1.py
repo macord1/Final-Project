3788bc6abe4cf911f1fcdd26933a196ff07a10e7
macord1 <54723752+macord1@users.noreply.github.com>
2020-05-01 13:50:30 -0400

    added data and ability to plot

--- Trial1.py
+++ Trial1.py
@@ -9,12 +9,10 @@ train_time = 5
 train_data = []
 test_data = []
 for i in range(0, int(train_time * fs)):
-    train_data.append(line[i])
+    train_data.append(float(line[i]))
 for i in range(int(train_time * fs) + 1, len(line)):
-    test_data.append(line[i])
+    test_data.append(float(line[i]))
 Train_t = np.arange(0.0, 5.0 - dt, dt)
 Test_t = np.arange(5.0, 25.0, dt)
-print(len(train_data))
-print(len(Train_t))
-# plt.plot(Train_t, train_data)
-# plt.show()
+plt.plot(Train_t, train_data)
+plt.show()
